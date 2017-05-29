7aa25328131d48d55ad34b5cca8b38865b18abf8
unknown <1034513@PBR0706.PUCMINAS.NET>
2017-05-29 15:07:19 -0300

    First commit

---
+++ Labirinto/Labirinto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinto
+{
+    class Labirinto
+    {
+
+        public void montaGrafo(){
+            
+        }
+
+        public void labirintoDoArquivo()
+        {
+            LerDoArquivo labirintoStr = new LerDoArquivo();
+            string[] labirinto = labirintoStr.getTexto();
+            for (int i = 0; i < labirinto.Length; i++) Console.WriteLine(labirinto[i]);
+
+            Console.WriteLine(labirinto[2][4]);
+        }
+    }
+}
---
+++ Labirinto/LerDoArquivo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Labirinto
+{
+    class LerDoArquivo
+    {
+        StreamReader sr = new StreamReader(@"labirinto.txt");
+        string[] labirinto;
+        public string[] getTexto()
+        {
+            string linha;
+            labirinto = new string[File.ReadLines(@"labirinto.txt").Count()];
+            for (int i = 0; (linha = sr.ReadLine()) != null; i++) labirinto[i] = linha;
+            return labirinto;
+        }
+    }
+}
